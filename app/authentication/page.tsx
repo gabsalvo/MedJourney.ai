22bcb6f23259e8cc1fc2cac16b976a94aaabb1f4
Gabriele <g.salvo4@studenti.unipi.it>
2025-03-22 12:15:04 +0100

    almost done

--- app/authentication/page.tsx
+++ app/authentication/page.tsx
@@ -5,6 +5,8 @@ import Link from "next/link"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { UserAuthForm } from "@/components/user-auth-form"
+import { FlickeringGrid } from "@/components/magicui/flickering-grid";
+
 
 export const metadata: Metadata = {
     title: "MedJourney.ai | Register",
@@ -28,7 +30,18 @@ export default function AuthenticationPage() {
             <div className="container relative hidden md:grid h-screen lg:max-w-none lg:grid-cols-2 lg:px-0">
                 {/* Sidebar - Hidden on mobile */}
                 <div className="relative hidden lg:flex h-full flex-col bg-muted p-10 text-white dark:border-r">
-                    <div className="absolute inset-0 bg-primary" />
+                    <div className="absolute inset-0 bg-zinc-950">
+                    <FlickeringGrid
+                        className="absolute inset-0 z-0 [mask-image:radial-gradient(450px_circle_at_center,white,transparent)]"
+                        squareSize={4}
+                        gridGap={6}
+                        color="#60A5FA"
+                        maxOpacity={0.5}
+                        flickerChance={0.1}
+                        height={800}
+                        width={900}
+                    />
+                    </div>
                     <div className="relative z-20 flex items-center text-lg font-medium">
                         <Link href="/" className={cn(buttonVariants({ variant: "ghost" }), "cursor-pointer hover:bg-zinc-700")}>
                         <Image
@@ -44,11 +57,9 @@ export default function AuthenticationPage() {
                     <div className="relative z-20 mt-auto">
                         <blockquote className="space-y-2">
                             <p className="text-lg">
-                                &ldquo;This library has saved me countless hours of work and
-                                helped me deliver stunning designs to my clients faster than
-                                ever before.&rdquo;
+                                &ldquo;Unveil new patterns in your patients data, save lives and push research forward in few simple clicks.&rdquo;
                             </p>
-                            <footer className="text-sm">Sofia Davis</footer>
+                            <footer className="text-sm">Gabriele Salvo</footer>
                         </blockquote>
                     </div>
                 </div>
